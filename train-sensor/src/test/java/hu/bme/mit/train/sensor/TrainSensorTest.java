4226243c56df7dd810063a38be6b2457410b46de
ThisOrange <toth.kristof.tko@gmail.com>
2023-03-20 16:17:11 +0100

    'Hiba_megoldva_Teszt_hozzaadva'

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,22 +3,30 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import static org.mockito.Mockito.*;
 
-import hu.bme.mit.train.interfaces.TrainSensor;
+
 
 public class TrainSensorTest {
     TrainSensor sensor;
+    TrainUser user;
+    TrainController controller;
+
 
     @Before
     public void before() {
-         
+         sensor = new TrainSensorImpl(controller, user);
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
-        int speedlimit = 10;
+        int speedlimit = 5;
         sensor.overrideSpeedLimit(speedlimit);
-        Assert.assertEquals(10, sensor.getSpeedLimit());
+        Assert.assertEquals(5, sensor.getSpeedLimit());
     }
 }
